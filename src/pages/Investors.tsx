906b9249010351a64a9cdd6256cb76c205e07a5f
thecreator23 <you@example.com>
2025-05-08 23:22:19 +0530

    final2

--- src/pages/Investors.tsx
+++ src/pages/Investors.tsx
@@ -5,7 +5,7 @@ import PageHeader from "@/components/common/PageHeader";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { FileText, Download } from "lucide-react";
 import placeholder from "../Assets/Logo/placeholder.jpg"; 
-import investorimg from "../Assets/Images/investor.jpeg";
+// import investorimg from "../Assets/Images/investor.jpeg";
 
 const Investors = () => {
   // Sample financial data for chart
@@ -35,7 +35,7 @@ const Investors = () => {
               <div className="flex flex-col gap-10 items-center justify-between mb-8">
                 <div className="flex flex-col md:flex-row items-center md:items-start w-full max-w-4xl mx-auto mb-4">
                   <img 
-                    src={placeholder}
+                    src={"/founder copy.jpeg"}
                     width={400}
                     alt="Founder 1" 
                     className="w-full md:w-[400px] h-auto md:mr-6 mb-4 md:mb-0 shadow-lg rounded-lg"
@@ -47,10 +47,11 @@ const Investors = () => {
 
                 <div className="flex flex-col md:flex-row items-center md:items-start w-full max-w-4xl mx-auto mb-4">
                   <div className="text-lg text-gray-700 md:flex-1 p-4">
-                  As the co-founder, <span className="font-bold">Mrs. Rajni Singh </span>  plays a key role in upholding the organization's ethical and social responsibilities. She ensures adherence to the highest standards while promoting sustainability, corporate citizenship, and global impact. Through a shared dedication to expanding access and leadership, Rajni Singh supports a mission that extends beyond business—to creating a positive, lasting difference worldwide.
-                  </div>
+                    <span className="font-bold">Mr. Sandip Gangopadhyay </span>
+                      A seasoned pharmaceutical industry professional with extensive leadership experience, Mr. Sandip Gangopadhyay brings strategic insight and global perspective as a valued shareholder. With a background spanning operations, business development, and international markets, he plays a crucial role in guiding the company’s vision for innovation, sustainability, and ethical growth.
+                    </div>
                   <img 
-                    src={placeholder}
+                    src={"/founder2.jpeg"}
                     width={400}
                     alt="Founder 1" 
                     className="w-full md:w-[400px] h-auto md:mr-6 mb-4 md:mb-0 shadow-lg rounded-lg"
@@ -59,22 +60,16 @@ const Investors = () => {
               </div>
             </div>
             <div className="max-w-4xl mx-auto">
-              <h2 className="text-3xl font-bold text-varsal-darkblue mb-6">Shareholder Information</h2>
-              <div className="w-20 h-1 bg-varsal-lightblue mb-8"></div>
-              
-              <p className="text-lg text-gray-700 mb-8">
-                Varsal Healthcare is committed to creating long-term value for our shareholders through ethical business practices, strategic growth initiatives, and transparent communication. We believe in maintaining open dialogues with our investors and providing regular updates on our performance and future outlook.
-              </p> 
+              <div className="w-20 h-1 bg-varsal-lightblue mb-8"></div> 
                 <div className="flex flex-col md:flex-row items-center md:items-start w-full max-w-4xl mx-auto mb-4">
                   <img 
-                    src={investorimg}
+                    src={placeholder}
                     width={400}
                     alt="Founder 1" 
                     className="w-full md:w-[400px] h-auto md:mr-6 mb-4 md:mb-0 shadow-lg rounded-lg"
                   />
                   <div className="text-lg text-gray-700 md:flex-1 p-4">
-                    <span className="font-bold">Mr. Sandip Gangopadhyay </span>
-                    A seasoned pharmaceutical industry professional with extensive leadership experience, Mr. Sandip Gangopadhyay brings strategic insight and global perspective as a valued shareholder. With a background spanning operations, business development, and international markets, he plays a crucial role in guiding the company’s vision for innovation, sustainability, and ethical growth.
+                    As the co-founder, <span className="font-bold">Mrs. Rajni Singh </span>  plays a key role in upholding the organization's ethical and social responsibilities. She ensures adherence to the highest standards while promoting sustainability, corporate citizenship, and global impact. Through a shared dedication to expanding access and leadership, Rajni Singh supports a mission that extends beyond business—to creating a positive, lasting difference worldwide.
                   </div>
                 </div>
             </div>
